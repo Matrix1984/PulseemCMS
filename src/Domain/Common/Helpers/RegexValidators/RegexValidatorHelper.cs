e39480775aaed3e015084f3bc9b52626772be93b
pentester1984@outlook.com <SxDVosm33MxgeOU5jXyn>
2023-08-16 21:28:39 +0300

    [Fix] Regex validation expressions

--- src/Domain/Common/Helpers/RegexValidators/RegexValidatorHelper.cs
+++ src/Domain/Common/Helpers/RegexValidators/RegexValidatorHelper.cs
@@ -9,9 +9,9 @@ namespace PulseemCMS.Domain.Common.Helpers.RegexValidators;
 public class RegexValidatorHelper
 {
     // Cellphone can start with 05 (len of 10 – local number) or with 9725 (len of 12 – International number) – it means that 972526292959=0526292959
-    public const string PHONE_NUMBER_REG_VALIDATOR = "/(9725|05)[0-9]{8,8}/";
+    public const string PHONE_NUMBER_REG_VALIDATOR = "(9725|05)[0-9]{8,8}";
 
-    public const string NAME_REG_VALIDATOR = "/[A-Za-z]{1,100}/";
+    public const string NAME_REG_VALIDATOR = "[A-Za-z]{1,100}";
 }
 
 
