0574c5f7a0e5f60dd41841d40755944ba32d6d81
pentester1984@outlook.com <SxDVosm33MxgeOU5jXyn>
2023-08-17 08:21:25 +0300

    [Fix] Update. SP fix.

--- src/Web/SqlScripts/UpdateClientSP.sql
+++ src/Web/SqlScripts/UpdateClientSP.sql
@@ -1,5 +1,12 @@
+USE [Pulseem]
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateClient]    Script Date: 17/08/2023 8:10:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
  
-CREATE PROCEDURE UpdateClient
+ALTER PROCEDURE [dbo].[UpdateClient]
 	   @Cellphone vARCHAR(12),
       @Email varchar(500),
    @ClientName varchar(100),
@@ -13,6 +20,8 @@ BEGIN
 
 	SET NOCOUNT ON; 
 
+	BEGIN TRANSACTION; 
+
     UPDATE Clients SET 
     Cellphone = @Cellphone,
     Email= @Email,
@@ -20,17 +29,13 @@ BEGIN
     EmailStatus= @EmailStatus,
     SmsStatus=@SmsStatus
     WHERE ClientId=@ClientId
-
-	IF( @SmsStatus = 1)
-	BEGIN
+	 
 
 		DECLARE @PhoneWithNoPrefix varchar(12) = IIF(LEFT(@Cellphone, 1)='0', SUBSTRING(@Cellphone,2,LEN(@Cellphone)),SUBSTRING(@Cellphone,4,LEN(@Cellphone)))
 
 	    UPDATE Clients SET  
-        SmsStatus=1
-        WHERE Cellphone LIKE CONCAT('%',@PhoneWithNoPrefix);
-    END;
+        SmsStatus=@SmsStatus
+        WHERE Cellphone LIKE CONCAT('%',@PhoneWithNoPrefix) and ClientId=@ClientId; 
 
 COMMIT TRANSACTION; 
 END
-GO
